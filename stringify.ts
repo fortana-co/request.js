40b14d54433222f5734752b19c596ead1bc08d84
Kyle Bebak <kylebebak@gmail.com>
2019-06-12 16:46:43 -0500

    Adds types to stringify

---
+++ stringify.ts
@@ -0,0 +1,41 @@
+// based on: https://github.com/Gozala/querystring/blob/master/encode.js
+const stringifyPrimitive = function(v: any): string | number {
+  switch (typeof v) {
+    case 'string':
+      return v
+
+    case 'boolean':
+      return v ? 'true' : 'false'
+
+    case 'number':
+      return isFinite(v) ? v : ''
+
+    default:
+      return ''
+  }
+}
+
+module.exports = function(obj) {
+  if (!obj) return ''
+
+  const sep = '&'
+  const eq = '='
+
+  return Object.keys(obj)
+    .map(function(k) {
+      const ks = encodeURIComponent(stringifyPrimitive(k)) + eq
+
+      if (Array.isArray(obj[k])) {
+        return obj[k]
+          .map(function(v) {
+            return ks + encodeURIComponent(stringifyPrimitive(v))
+          })
+          .join(sep)
+      } else {
+        if (obj[k] === undefined) return ''
+        return ks + encodeURIComponent(stringifyPrimitive(obj[k]))
+      }
+    })
+    .filter(Boolean)
+    .join(sep)
+}
